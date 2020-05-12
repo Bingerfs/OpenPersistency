2fe4124daca93eb245f00ef3afd7f91706f5e40e
Hector Hidalgo <marce.753@gmail.com>
2020-05-12 10:51:19 -0400

    Fixed

--- src/main/java/it/xpug/birthday_greetings/Greeting.java
+++ src/main/java/it/xpug/birthday_greetings/Greeting.java
@@ -2,12 +2,12 @@ package it.xpug.birthday_greetings;
 
 public class Greeting {
     public String subject;
-    public String body=  "Happy Birthday, dear %NAME%";
+    public String body;
     public String recipient;
 
     public Greeting(Employee employee) {
         subject="Happy Birthday!";
-        body.replace("%NAME%", employee.getFirstName());
+        body= "Happy Birthday, dear %NAME%".replace("%NAME%", employee.getFirstName());
         recipient = employee.getEmail();
     }
     
