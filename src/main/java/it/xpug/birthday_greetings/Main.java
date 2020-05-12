ab3d800c86ad78de7ea67836bcb7fb9b8912e784
Hector Hidalgo <marce.753@gmail.com>
2020-05-12 09:09:34 -0400

    First commit

---
+++ src/main/java/it/xpug/birthday_greetings/Main.java
@@ -0,0 +1,18 @@
+package it.xpug.birthday_greetings;
+
+import java.io.*;
+import java.text.ParseException;
+
+import javax.mail.*;
+import javax.mail.internet.*;
+
+public class Main {
+
+	public static void main(String[] args) throws AddressException, IOException, ParseException, MessagingException {
+		/*EmployeeRepository employeeRepository = new FileEmployeeRepository("employee_data.txt");
+		EmailService emailService = new SmtpEmailService("localhost", 25);
+		BirthdayService service = new BirthdayService();
+		service.sendGreetings(OurDate.today());*/
+	}
+
+}
